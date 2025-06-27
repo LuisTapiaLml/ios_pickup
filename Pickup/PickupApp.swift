1322a2c6ee24204538e0721c6dfeffefa60135eb
luis tapia <luistapialml@gmail.com>
2025-06-27 00:37:47 -0600

    Initial Commit

---
+++ Pickup/PickupApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickupApp.swift
+//  Pickup
+//
+//  Created by Luis Enrique Perez Tapia on 27/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PickupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
